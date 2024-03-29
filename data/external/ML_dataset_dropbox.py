31c926b8d904d73e7ab923ce7d3c3391a927088b
estheraruti <128743038+estheraruti@users.noreply.github.com>
2024-03-29 00:15:42 -0400

    change to read

--- data/external/ML_dataset_dropbox.py
+++ data/external/ML_dataset_dropbox.py
@@ -1,8 +1,10 @@
 import dropbox
 import pandas as pd
+import io 
 
 # Dropbox access token
-DROPBOX_ACCESS_TOKEN = 'sl.ByVh8odFyt4CaBJ2qq_bOUjewfR3X3Cbhx-2-QooBJaX9c3hTRCiR0Kw7G4MC35Hm82Xkb1-QdsmYqMukcHE7vJ2n00w4Rg-TcsVrjBLuAPGleU2L1TijWHKTwM9BhVflnlEvaxEAas3'
+DROPBOX_ACCESS_TOKEN = 'sl.ByU4ITWZF9fhmTE_RBP5Ungczy859C4t9k16Yd029DLY4LGwAZgVLEd4K7Po3GugXFJx0JL6FIwgJzzgFAVwf9683rEQDbD1ao2_tmJ36vscnYi3HARCcibXBWYBxDOG5z-JfgyARwO6'
+
 # Initialize Dropbox client
 dbx = dropbox.Dropbox(DROPBOX_ACCESS_TOKEN)
 
@@ -12,14 +14,9 @@ dropbox_file_path = '/fake_real_ML_project_dataset.csv'
 # Download the file
 metadata, response = dbx.files_download(dropbox_file_path)
 
-# Save the downloaded file
-with open('data.csv', 'wb') as f:
-    f.write(response.content)
-
-# Read the CSV file
-df = pd.read_csv('data.csv')
+# Read the CSV file from the response content using io.BytesIO
+df = pd.read_csv(io.BytesIO(response.content))
 
 # Show the results
 print("Dataframe loaded from Dropbox:")
 print(df)
-
