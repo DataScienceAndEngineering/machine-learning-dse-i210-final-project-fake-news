d59d844b5344344dd4e38080a06416b638d6d769
Esther Aruti <estheraruti@gmail.com>
2024-04-13 20:34:51 -0400

    added separate spacy notebook

--- data/external/ML_dataset_dropbox.py
+++ data/external/ML_dataset_dropbox.py
@@ -1,12 +1,11 @@
 import dropbox
 import pandas as pd
-import io 
-
-# Dropbox access token
-DROPBOX_ACCESS_TOKEN = 'sl.ByU4ITWZF9fhmTE_RBP5Ungczy859C4t9k16Yd029DLY4LGwAZgVLEd4K7Po3GugXFJx0JL6FIwgJzzgFAVwf9683rEQDbD1ao2_tmJ36vscnYi3HARCcibXBWYBxDOG5z-JfgyARwO6'
+import io
+import os 
+from config import dropbox_access_token
 
 # Initialize Dropbox client
-dbx = dropbox.Dropbox(DROPBOX_ACCESS_TOKEN)
+dbx = dropbox.Dropbox(dropbox_access_token)
 
 # Dropbox file path
 dropbox_file_path = '/fake_real_ML_project_dataset.csv'
@@ -19,4 +18,4 @@ df = pd.read_csv(io.BytesIO(response.content))
 
 # Show the results
 print("Dataframe loaded from Dropbox:")
-print(df)
+print(df.head())
